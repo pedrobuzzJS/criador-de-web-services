09d00bb6f15d96009b252c625b18203a363cf6a0
pedrobuzzJS <pedro.buzzi@unidavi.edu.br>
2022-07-04 23:30:01 -0300

    ajeitando o export

--- frontend/src/routes/index.routes.tsx
+++ frontend/src/routes/index.routes.tsx
@@ -3,7 +3,9 @@ import React from "react";
 const Routes: React.FC = () => {
     return (
         <div>
-            
+            <span>Ol[a</span>
         </div>
     )
 }
+
+export default Routes;

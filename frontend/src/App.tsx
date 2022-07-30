0dfc8fc165a01850faa1b35b284687e68bef658e
pedrobuzzJS <pedro.buzzi@unidavi.edu.br>
2022-07-30 02:09:37 -0300

    globalStyle no index.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { Menu } from "./components/Menu/styles";
-import GlobalStyles from "./styles/GlobalStyles";
+import Layout from "./components/Layout/Layout";
 
 // interface Usuario {
 //   cep: string;
@@ -23,8 +22,7 @@ const App: React.FC = () => {
 
   return (
     <>
-      <Menu />
-      <GlobalStyles />
+      <Layout />
     </>
   )
 };
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import GlobalStyles from './styles/GlobalStyles';
 
 
 const root = ReactDOM.createRoot(
@@ -10,6 +11,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
       <App />
+      <GlobalStyles />
   </React.StrictMode>,
 );
 

4bf17149e133b94fe7c58f3d48e2592633fd2172
pedrobuzzJS <pedro.buzzi@unidavi.edu.br>
2022-11-07 20:25:04 -0300

    gerador de comandos sql

--- backend/src/modules/ObjToSqlController.ts
+++ backend/src/modules/ObjToSqlController.ts
@@ -2,7 +2,7 @@ import { client } from "../infra/prisma/client";
 import { Request, Response } from 'express';
 import { ListWebServicesObjUseCase } from '../useCases/webServicesOjbUseCases/ListWebServices/ListWebServicesObjUseCase';
 import { WebServiceObj } from "@prisma/client";
-
+import { ListTableUseCase } from "../useCases/tabelaUseCases/listTable/ListTableUseCase";
 export default class ObjToSqlController {
     static async handle(request: Request, response: Response) {
         const { id } = request.query;
@@ -31,13 +31,41 @@ export default class ObjToSqlController {
     async ObjToSql(obj: any, id: number) {
         const tableId = obj.tableId;
         const webServiceId = obj.webServiceId;
-    const objJson: any[] = obj.data;
+        const objJson: any[] = obj.data;
+        const listTableUseCase = new ListTableUseCase();
         let select = "SELECT ";
-        let from = "FROM";
-
+        let columns = "";
+        let from = "FROM ";
+        let tb = await listTableUseCase.getById(tableId);
+        from = from + tb[0].nome;
         await objJson.forEach(el => {
-            console.log(el.nome);
-            select 
+            columns = columns + el.nome + " ,";
         })
+        columns = columns.substring(0, columns.length - 1);
+        select = select + columns + from;
+        await console.log(select);
+
+        try {
+            const updateWebServiceObj = await client.webServiceObj.update({
+                where: {
+                    id: Number(id)
+                },
+                data: {
+                    sql: select,
+                }
+            });
+            if (updateWebServiceObj) {
+                // return response.json(
+                //     {
+                //         "msg" : "Successes"
+                //     }
+                // );
+            };
+        } catch (error) {
+            // return response.status(500).json({
+                // message: error
+            // });
+        };
+
     };
 };
--- frontend/src/context/formContext.tsx
+++ frontend/src/context/formContext.tsx
@@ -1,6 +1,5 @@
 import React, {
     useState,
-    useEffect,
     createContext,
     useContext,
     PropsWithChildren
